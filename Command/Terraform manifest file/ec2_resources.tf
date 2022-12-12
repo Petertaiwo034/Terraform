c8a0f13772feef4d1690077dbddca16cce0468a1
Petertaiwo034 <111324590+Petertaiwo034@users.noreply.github.com>
2022-12-12 17:05:22 -0600

    Update ec2_resources.tf

--- Command/Terraform manifest file/ec2_resources.tf
+++ Command/Terraform manifest file/ec2_resources.tf
@@ -1,7 +1,7 @@
 Then create a Resources file ec2.tf
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = var.ec2_ami_id
+  ami           = data.aws_ami.amazon_linux.id #(data. resource type. name you used to save the resource (resource name). id)
   instance_type = var.ec2_instance_count
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   
