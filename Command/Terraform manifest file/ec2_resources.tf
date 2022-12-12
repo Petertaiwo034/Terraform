a946b85731e9113431eb5a48193fdd6c02d94186
Petertaiwo034 <111324590+Petertaiwo034@users.noreply.github.com>
2022-12-12 17:06:32 -0600

    Update ec2_resources.tf

--- Command/Terraform manifest file/ec2_resources.tf
+++ Command/Terraform manifest file/ec2_resources.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2demo" {
   ami           = data.aws_ami.amazon_linux.id #(data. resource type. name you used to save the resource (resource name). id)
   instance_type = var.ec2_instance_count
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  
+  key_name = var.key_pair
   tag = {
    "Name" = "Terraform server"
    }
