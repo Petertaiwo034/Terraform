4079d8d46a9ff167cde267eff774907313e61d44
Petertaiwo034 <111324590+Petertaiwo034@users.noreply.github.com>
2022-12-13 14:16:38 -0600

    Update output.tf

--- Command/output.tf
+++ Command/output.tf
@@ -1,6 +1,6 @@
 #Create an output file e.g output.tf
 output "instance_publicip" {
   description = "Ec2 public Ip"
-  value = aws_instance.ec2demo.public_ip #(Name of your resources. Name you used to store it. and the resource you want to output)
+  value = aws_instance.ec2demo.public_ip #(Resources type.Resources Name.and the resource you want to output)
 }
 #After you run terraform apply -auto-apply it will output the public ip address. 
